2281d6c4a67366df9d496c0e6217a435c0bbc8ba
CloetWiebe <wiebe.cloet@student.howest.be>
2021-12-04 12:32:51 +0100

    Added script

---
+++ script/script.js
@@ -0,0 +1,14 @@
+const data ={
+	"top3champs": [{
+		"name": "Shen",
+		"mastery": 107007
+	}, {
+		"name": "Syndra",
+		"mastery": 69442
+	}, {
+		"name": "Maokai",
+		"mastery": 42545
+	}],
+	"totalmastery": 1211560
+}
+
